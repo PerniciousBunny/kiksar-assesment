9008217234a244648c71deb82cab97195b5465ce
vedha <vedhagnanam.moorty-barnabas@realworld-one.com>
2021-11-27 14:22:32 +0530

    added grid layout and JSON

---
+++ Filter System/Assets/Scripts/Base/Item.cs
@@ -0,0 +1,10 @@
+
+[System.Serializable]
+public class Item
+{   
+    public string Name;
+    public string Category;
+    public string SubCategory;
+    public int Price;
+
+}
---
+++ Filter System/Assets/Scripts/Base/ItemsList.cs
@@ -0,0 +1,7 @@
+using System.Security.AccessControl;
+using System.Collections.Generic;
+[System.Serializable]
+public class ItemsList
+{
+    List<Item> Items = new List<Item>();    
+}
